daed10ca8cde97248f7a7c85b0e12f0baaee6da1
Christopher Durand <christopher.durand@rwth-aachen.de>
2017-08-31 00:53:06 +0200

    [software/onboard] Define logger device for XPCC_LOG_* streams

---
+++ software/onboard/src/Logger.cpp
@@ -0,0 +1,30 @@
+/* Logger.cpp
+ *
+ * Copyright (C) 2017 Christopher Durand
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "Logger.hpp"
+
+#include <xpcc/debug/logger.hpp>
+
+using viper::onboard::LoggerDevice;
+
+LoggerDevice loggerDevice;
+
+xpcc::log::Logger xpcc::log::debug(loggerDevice);
+xpcc::log::Logger xpcc::log::info(loggerDevice);
+xpcc::log::Logger xpcc::log::warning(loggerDevice);
+xpcc::log::Logger xpcc::log::error(loggerDevice);
---
+++ software/onboard/src/Logger.hpp
@@ -0,0 +1,34 @@
+/* Logger.hpp
+ *
+ * Copyright (C) 2017 Christopher Durand
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef VIPER_ONBOARD_LOGGER_HPP
+#define VIPER_ONBOARD_LOGGER_HPP
+
+#include "../hardware_rev1.hpp"
+
+namespace viper
+{
+namespace onboard
+{
+
+using LoggerDevice = xpcc::IODeviceWrapper<Board::Ui::DebugUart, xpcc::IOBuffer::BlockIfFull>;
+
+}
+}
+
+#endif // VIPER_ONBOARD_LOGGER_HPP
