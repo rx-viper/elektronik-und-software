e2b106ede445e76a7b09842e0c0218bc206b98f2
Raphael Lehmann <raphael@rleh.de>
2017-10-25 19:48:47 +0200

    [communication] commit generated packet hpp/cpp files

--- software/common/communication/generated/Packets.hpp
+++ software/common/communication/generated/Packets.hpp
@@ -1,6 +1,10 @@
 #ifndef VIPER_PACKETS_HPP__GENERATED
 #define VIPER_PACKETS_HPP__GENERATED
 
+/*
+ *  This is an auto-generated file. DO NOT EDIT!
+ */
+
 #include <array>
 #include <cstddef>
 #include <cstdint>
@@ -9,6 +13,230 @@
 #include <DataWriter.hpp>
 #include <PacketGroup.hpp>
 
-#error "This is a dummy file, run the packet generator!"
+/// @file Packets.hpp
+
+namespace viper
+{
+namespace packet
+{
+
+using communication::PacketGroup;
+using communication::DataReader;
+using communication::DataWriter;
+
+/// @enum TestEnum
+enum class TestEnum : uint8_t
+{
+    Enum0,
+    Enum1,
+    Enum2
+};
+
+/// Packet IceTemperatureLS
+struct IceTemperatureLS
+{
+    static constexpr uint8_t PacketID{0x0};
+    static constexpr size_t PacketSize{112};
+    
+    mutable uint32_t sequenceNumber;
+    std::array<int32_t, 27> temperatures;
+
+    bool read(DataReader& reader);
+    bool write(DataWriter& writer) const;
+
+    constexpr inline size_t size() const { return PacketSize; }
+};
+
+/// Packet IceTemperatureHS
+struct IceTemperatureHS
+{
+    static constexpr uint8_t PacketID{0x1};
+    static constexpr size_t PacketSize{112};
+    
+    mutable uint32_t sequenceNumber;
+    std::array<int32_t, 27> temperatures;
+
+    bool read(DataReader& reader);
+    bool write(DataWriter& writer) const;
+
+    constexpr inline size_t size() const { return PacketSize; }
+};
+
+/// Packet OtherTemperatureLS
+struct OtherTemperatureLS
+{
+    static constexpr uint8_t PacketID{0x2};
+    static constexpr size_t PacketSize{14};
+    
+    mutable uint32_t sequenceNumber;
+    std::array<uint16_t, 5> temperatures;
+
+    bool read(DataReader& reader);
+    bool write(DataWriter& writer) const;
+
+    constexpr inline size_t size() const { return PacketSize; }
+};
+
+/// Packet OtherTemperatureHS
+struct OtherTemperatureHS
+{
+    static constexpr uint8_t PacketID{0x3};
+    static constexpr size_t PacketSize{14};
+    
+    mutable uint32_t sequenceNumber;
+    std::array<uint16_t, 5> temperatures;
+
+    bool read(DataReader& reader);
+    bool write(DataWriter& writer) const;
+
+    constexpr inline size_t size() const { return PacketSize; }
+};
+
+/// Packet HpPenetrationDepthLS
+struct HpPenetrationDepthLS
+{
+    static constexpr uint8_t PacketID{0x4};
+    static constexpr size_t PacketSize{16};
+    
+    mutable uint32_t sequenceNumber;
+    std::array<int32_t, 3> depth;
+
+    bool read(DataReader& reader);
+    bool write(DataWriter& writer) const;
+
+    constexpr inline size_t size() const { return PacketSize; }
+};
+
+/// Packet HpPenetrationDepthHS
+struct HpPenetrationDepthHS
+{
+    static constexpr uint8_t PacketID{0x5};
+    static constexpr size_t PacketSize{16};
+    
+    mutable uint32_t sequenceNumber;
+    std::array<int32_t, 3> depth;
+
+    bool read(DataReader& reader);
+    bool write(DataWriter& writer) const;
+
+    constexpr inline size_t size() const { return PacketSize; }
+};
+
+/// Packet PressureLS
+struct PressureLS
+{
+    static constexpr uint8_t PacketID{0x6};
+    static constexpr size_t PacketSize{24};
+    
+    mutable uint32_t sequenceNumber;
+    std::array<int32_t, 5> values;
+
+    bool read(DataReader& reader);
+    bool write(DataWriter& writer) const;
+
+    constexpr inline size_t size() const { return PacketSize; }
+};
+
+/// Packet PressureHS
+struct PressureHS
+{
+    static constexpr uint8_t PacketID{0x7};
+    static constexpr size_t PacketSize{84};
+    
+    mutable uint32_t sequenceNumber;
+    std::array<int32_t, 20> values;
+
+    bool read(DataReader& reader);
+    bool write(DataWriter& writer) const;
+
+    constexpr inline size_t size() const { return PacketSize; }
+};
+
+/// Packet HpTemperatureLS
+struct HpTemperatureLS
+{
+    static constexpr uint8_t PacketID{0x8};
+    static constexpr size_t PacketSize{16};
+    
+    mutable uint32_t sequenceNumber;
+    std::array<int32_t, 3> temperatures;
+
+    bool read(DataReader& reader);
+    bool write(DataWriter& writer) const;
+
+    constexpr inline size_t size() const { return PacketSize; }
+};
+
+/// Packet HpTemperatureHS
+struct HpTemperatureHS
+{
+    static constexpr uint8_t PacketID{0x9};
+    static constexpr size_t PacketSize{16};
+    
+    mutable uint32_t sequenceNumber;
+    std::array<int32_t, 3> temperatures;
+
+    bool read(DataReader& reader);
+    bool write(DataWriter& writer) const;
+
+    constexpr inline size_t size() const { return PacketSize; }
+};
+
+/// Packet Status
+struct Status
+{
+    static constexpr uint8_t PacketID{0xa};
+    static constexpr size_t PacketSize{11};
+    
+    mutable uint32_t sequenceNumber;
+    uint32_t time;
+    uint8_t lo;
+    uint8_t soe;
+    uint8_t sods;
+
+    bool read(DataReader& reader);
+    bool write(DataWriter& writer) const;
+
+    constexpr inline size_t size() const { return PacketSize; }
+};
+
+/// Packet TestPacket1
+struct TestPacket1
+{
+    static constexpr uint8_t PacketID{0xff};
+    static constexpr size_t PacketSize{6};
+    
+    mutable uint32_t sequenceNumber;
+    std::array<TestEnum, 2> testArray;
+
+    bool read(DataReader& reader);
+    bool write(DataWriter& writer) const;
+
+    constexpr inline size_t size() const { return PacketSize; }
+};
+
+/// Packet TestPacket2
+struct TestPacket2
+{
+    static constexpr uint8_t PacketID{0xfe};
+    static constexpr size_t PacketSize{9};
+    
+    mutable uint32_t sequenceNumber;
+    int32_t test1;
+    TestEnum test2;
+
+    bool read(DataReader& reader);
+    bool write(DataWriter& writer) const;
+
+    constexpr inline size_t size() const { return PacketSize; }
+};
+
+/// PacketGroup GroundstationPackets
+using GroundstationPackets = PacketGroup<IceTemperatureLS, IceTemperatureHS, OtherTemperatureLS, OtherTemperatureHS, HpPenetrationDepthLS, HpPenetrationDepthHS, PressureLS, PressureHS, HpTemperatureLS, HpTemperatureHS, Status>;
+/// PacketGroup TestPackets
+using TestPackets = PacketGroup<TestPacket1, TestPacket2>;
+
+}
+}
 
 #endif
