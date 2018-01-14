cc710cb7e2859c31a7cea00227f39ae579d83394
Raphael Lehmann <raphael@rleh.de>
2018-01-14 17:40:36 +0100

    [software/groundstation] SQL database tables

---
+++ software/groundstation/sql/database-tables.sql
@@ -0,0 +1,114 @@
+﻿CREATE TABLE IF NOT EXISTS experiment(
+   _id BIGSERIAL PRIMARY KEY,
+   _database_time TIMESTAMP NOT NULL DEFAULT NOW(),
+   id INTEGER UNIQUE NOT NULL,
+   groundstation_time TIMESTAMP NOT NULL
+);
+
+DROP TYPE IF EXISTS SAMPLE_FREQ CASCADE;
+CREATE TYPE SAMPLE_FREQ AS ENUM ('LOW', 'HIGH');
+
+CREATE TABLE IF NOT EXISTS data_ice_sample_temperature(
+   _id BIGSERIAL PRIMARY KEY,
+   _database_time TIMESTAMP NOT NULL DEFAULT NOW(),
+   ice_sample_id INTEGER NOT NULL,
+   sensor_id INTEGER NOT NULL,
+   sample_freqency SAMPLE_FREQ NOT NULL,
+   value DOUBLE PRECISION NOT NULL,
+   error BOOL DEFAULT NULL,
+   experiment_time INTEGER NOT NULL,
+   groundstation_time TIMESTAMP NOT NULL,
+   experiment_id INTEGER REFERENCES experiment(id) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS data_heat_probe_temperature(
+   _id BIGSERIAL PRIMARY KEY,
+   _database_time TIMESTAMP NOT NULL DEFAULT NOW(),
+   sensor_id INTEGER NOT NULL,
+   sample_freqency SAMPLE_FREQ,
+   value DOUBLE PRECISION NOT NULL,
+   error BOOL DEFAULT NULL,
+   experiment_time INTEGER NOT NULL,
+   groundstation_time TIMESTAMP NOT NULL,
+   experiment_id INTEGER REFERENCES experiment(id) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS data_other_temperature(
+   _id BIGSERIAL PRIMARY KEY,
+   _database_time TIMESTAMP NOT NULL DEFAULT NOW(),
+   sensor_id INTEGER NOT NULL,
+   sample_freqency SAMPLE_FREQ,
+   value DOUBLE PRECISION NOT NULL,
+   error BOOL DEFAULT NULL,
+   experiment_time INTEGER NOT NULL,
+   groundstation_time TIMESTAMP NOT NULL,
+   experiment_id INTEGER REFERENCES experiment(id) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS data_heat_probe_penetration_depth(
+   _id BIGSERIAL PRIMARY KEY,
+   _database_time TIMESTAMP NOT NULL DEFAULT NOW(),
+   sensor_id INTEGER NOT NULL,
+   sample_freqency SAMPLE_FREQ,
+   value INTEGER NOT NULL,
+   error BOOL DEFAULT NULL,
+   experiment_time INTEGER NOT NULL,
+   groundstation_time TIMESTAMP NOT NULL,
+   experiment_id INTEGER REFERENCES experiment(id) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS data_motor_position(
+   _id BIGSERIAL PRIMARY KEY,
+   _database_time TIMESTAMP NOT NULL DEFAULT NOW(),
+   sensor_id INTEGER NOT NULL,
+   sample_freqency SAMPLE_FREQ,
+   value INTEGER NOT NULL,
+   error BOOL DEFAULT NULL,
+   experiment_time INTEGER NOT NULL,
+   groundstation_time TIMESTAMP NOT NULL,
+   experiment_id INTEGER REFERENCES experiment(id) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS data_pressure(
+   _id BIGSERIAL PRIMARY KEY,
+   _database_time TIMESTAMP NOT NULL DEFAULT NOW(),
+   sensor_id INTEGER NOT NULL,
+   sample_freqency SAMPLE_FREQ,
+   pressure DOUBLE PRECISION NOT NULL,
+   temperature DOUBLE PRECISION NOT NULL,
+   error BOOL DEFAULT NULL,
+   experiment_time INTEGER NOT NULL,
+   groundstation_time TIMESTAMP NOT NULL,
+   experiment_id INTEGER REFERENCES experiment(id) NOT NULL
+);
+
+DROP TYPE IF EXISTS EVENT_FLAG CASCADE;
+CREATE TYPE EVENT_FLAG AS ENUM ('LO', 'SOE', 'SODS', 'TEST_MODE', 'MOTOR_HOME');
+CREATE TABLE IF NOT EXISTS data_flags(
+   _id BIGSERIAL PRIMARY KEY,
+   _database_time TIMESTAMP NOT NULL DEFAULT NOW(),
+   sensor_id EVENT_FLAG NOT NULL,
+   value BOOL NOT NULL,
+   experiment_time INTEGER NOT NULL,
+   groundstation_time TIMESTAMP NOT NULL,
+   experiment_id INTEGER REFERENCES experiment(id) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS data_state_machine(
+   _id BIGSERIAL PRIMARY KEY,
+   _database_time TIMESTAMP NOT NULL DEFAULT NOW(),
+   state INTEGER NOT NULL,
+   experiment_time INTEGER NOT NULL,
+   groundstation_time TIMESTAMP NOT NULL,
+   experiment_id INTEGER REFERENCES experiment(id) NOT NULL
+);
+
+DROP TYPE IF EXISTS SER_DIRECTION CASCADE;
+CREATE TYPE SER_DIRECTION AS ENUM ('RX', 'TX');
+CREATE TABLE IF NOT EXISTS raw_data(
+   _id BIGSERIAL PRIMARY KEY,
+   _database_time TIMESTAMP NOT NULL DEFAULT NOW(),
+   groundstation_time TIMESTAMP NOT NULL,
+   serial_data BYTEA NOT NULL,
+   direction SER_DIRECTION DEFAULT 'RX'
+);
