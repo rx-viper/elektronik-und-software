bcc4516116d5741aa96fba70e64b73d668dfd783
Christopher Durand <christopher.durand@rwth-aachen.de>
2018-01-23 12:03:45 +0100

    WIP

--- software/pi/camera/FileUtil.cpp
+++ software/pi/camera/FileUtil.cpp
@@ -4,6 +4,23 @@
 #include <iostream>
 #include <sys/statvfs.h>
 
+using std::string_literals;
+
+std::string createExperimentDirectory()
+{
+	std::string command = "mkdir -p "s + PiDataPath;
+	system(command.c_str());
+	
+	// TODO: implement
+}
+
+bool directoryExists(std::string path)
+{
+	struct stat sb;
+
+	return stat(path.c_str(), &sb) == 0 && S_ISDIR(sb.st_mode);
+}
+
 std::string nextFilename(const std::string& prefix, const std::string& suffix)
 {
 	char number[7];
